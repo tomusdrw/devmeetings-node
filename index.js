24f520c2a73d761c5728d0729058937e373d70a3
Tomasz Drwięga <tomusdrw@gmail.com>
2017-08-08 19:19:46 +0200

    Wykrywanie blokowania event loopa. [blocked]

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
+const blocked = require('blocked')
 const fs = require('fs')
 const http = require('http')
-const port = process.env.PORT || 3000
 
+const port = process.env.PORT || 3000
 const file = './file'
 
+// 3/ Blocked poinformuje nas o tym, że event loop był zablokowany.
+blocked((ms) => {
+  console.log(`Event loop was blocked for ${ms}.`)
+}, { threshold: 20 })
+
 http.createServer((req, res) => {
-  // 4/ W przypadku requestów na /stream wysyłamy stream
   if (req.url === '/stream') {
     fs.createReadStream(file).pipe(res)
     return
   }
 
-  // 4/ Czytam plik synchronicznie
-  if (req.url === '/sync') {
-    res.end(fs.readFileSync(file, 'utf8'))
+  // 3/ Możemy spróbować modyfikować bufory streama.
+  if (req.url === '/stream2') {
+    fs.createReadStream(file, { highWaterMark: 2**20 }).pipe(res)
     return
   }
 
-  // 12/ Czytamy plik asynchronicznie
+  if (req.url === '/sync') {
+    res.end(fs.readFileSync(file))
+    return
+  }
+
   if (req.url === '/async') {
     fs.readFile(file, 'utf8', (err, data) => {
       if (err) {
